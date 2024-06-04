2c9c0cab5d9c2c88025e3101719e108879917850
Subrata Sarkar <tccdcc8@gmail.com>
2024-06-04 23:50:04 +0600

    h

--- src/Pages/UserPages/AllReview.jsx
+++ src/Pages/UserPages/AllReview.jsx
@@ -3,9 +3,8 @@ import useAuth from "../../hooks/useAuth";
 import ReviewCard from "./ReviewCard";
 
 const AllReview = () => {
-    const {user}=useAuth()
-    const [reviews, setReviews] = useState([]);
-    console.log(reviews)
+  const { user } = useAuth();
+  const [reviews, setReviews] = useState([]);
   useEffect(() => {
     fetch(`https://digitalfurnitureserver.vercel.app/reviews/me/${user?.email}`)
       .then((response) => response.json())
@@ -14,9 +13,10 @@ const AllReview = () => {
   return (
     <div>
       <div className="flex gap-3 flex-wrap">
-        {reviews && reviews.map((review) => (
-          <ReviewCard key={review._id} review={review}></ReviewCard>
-        ))}
+        {reviews &&
+          reviews.map((review) => (
+            <ReviewCard key={review._id} review={review}></ReviewCard>
+          ))}
       </div>
     </div>
   );
